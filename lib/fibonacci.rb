beb92812788c4567dc2b5be9f365b8d7d49c3fb5
Jessica <jessica.homet@gmail.com>
2019-04-25 09:54:34 -0700

    Implement fibonacci (poorly)

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -7,6 +7,37 @@
 
 # Time complexity: ?
 # Space complexity: ?
+
+## this works but feels bad
+# def fibonacci(n)
+#   number_one = 0
+#   number_two = 1
+#   number_three = nil
+
+#   return 0 if n == 0
+#   raise ArgumentError if n < 0
+
+#   (n - 1).times do |i|
+#     number_three = number_one + number_two
+#     number_one = number_two
+#     number_two = number_three
+#   end
+
+#   return number_three
+# end
+
 def fibonacci(n)
-  raise NotImplementedError
+  if n == 0
+    return 0
+  elsif n.to_i == 0 || n < 0
+    raise ArgumentError
+  end
+
+  array = [0, 1]
+  (n - 1).times do |i|
+    number = array[-1] + array[-2]
+    array << number
+  end
+
+  return array[-1]
 end
